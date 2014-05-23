0c6545e86eb7994997f53d43fa841b053ee4e8e8
josestrk <josenass22@gmail.com>
2014-05-23 08:56:02 +0200

    Primer ejercicio

---
+++ CartaMenu/src/Paso.java
@@ -0,0 +1,14 @@
+
+public class Paso {
+	private String descripcion;
+	private Tiempo tiempoPreparacion;
+	
+	public Paso(){
+		
+	}
+	public Paso(String descrip, Tiempo tiempopre){
+		this.descripcion=descrip;
+		this.tiempoPreparacion=tiempopre;
+	}
+	
+}
