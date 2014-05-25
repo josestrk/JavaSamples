22338a4bc85998d0e8c0b5707e18b6ed24f9d389
Jose <Jose@Jose-HP>
2014-05-25 19:22:50 +0200

    subida sin errores

--- CartaMenu/src/Menu.java
+++ CartaMenu/src/Menu.java
@@ -1,4 +1,18 @@
+import java.util.*;
+
 
 public class Menu {
-
+	private TreeSet<FastFoodeable> fastFood;
+	
+	
+	public Tiempo tiempoTotal(){
+		Tiempo t= new Tiempo();
+		Iterator it=fastFood.iterator();
+		
+		while(it.hasNext()){
+			t.suma(((FastFoodeable) it.next()).tiempoPreparacion());
+		}
+		
+		return t;
+	}
 }
--- CartaMenu/src/Tiempo.java
+++ CartaMenu/src/Tiempo.java
@@ -1,4 +1,32 @@
 
 public class Tiempo {
+	private int h;
+	private int m;
+	private int s;
 
+	public Tiempo() {
+		h=0;
+		m=0;
+		s=0;
+	}
+	
+	public Tiempo(Tiempo tiempoTotal) {
+		// TODO Auto-generated constructor stub
+	}
+	
+	public void suma(Tiempo t){
+		this.h+=t.h;
+		
+	}
+
+	public double media(int div) {
+		int sumM=h*60;
+		double media;
+		sumM+=m;
+		if(s>30){
+			sumM++;
+		}
+		media=sumM/div;
+		return media;
+	}
 }
