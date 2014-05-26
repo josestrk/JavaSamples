bf420b071b53a614a2bb08454c75dc18268fe1fb
Alevale <alexvales111@gmail.com>
2014-05-26 13:12:10 +0200

    Sin fallos

--- CartaMenu/src/AdminBBDD/insertPersonalizado.java
+++ CartaMenu/src/AdminBBDD/insertPersonalizado.java
@@ -13,12 +13,12 @@ public class insertPersonalizado {
 		
 		System.out.println("Introduzca tabla");
 		Scanner sca=new Scanner(System.in);
-		s[i]=sca.next();//con esto nos hemos guardado en primera posicion el nombre de la tabla
+		s[i]=sca.nextLine();//con esto nos hemos guardado en primera posicion el nombre de la tabla
 		i++;
 		
 		while(a){
 			System.out.println("Introduzca Nombre de fila si desea salir ->(S/N)");
-			escaner=sca.next();
+			escaner=sca.nextLine();
 			if(escaner.equals("S") || escaner.equals("s") ){
 				a=false;
 			}else{
@@ -30,7 +30,7 @@ public class insertPersonalizado {
 		
 		
 		for(int j=1; j<i;j++){
-			System.out.println("INSERT INTO "+s[0]+" VALUES (IdEstacion.NEXTVAL, "+s[j]+");");
+			System.out.println("INSERT INTO "+s[0]+" VALUES (IdEstacion.NEXTVAL, '"+s[j]+"');");
 		}
 		
 		
