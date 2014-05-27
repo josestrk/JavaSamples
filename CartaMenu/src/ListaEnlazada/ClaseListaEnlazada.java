cc4d66de9cccbc83282f42f1841d30c4c416a611
josestrk <josenass22@gmail.com>
2014-05-27 10:31:09 +0200

    ListaEnlazada

---
+++ CartaMenu/src/ListaEnlazada/ClaseListaEnlazada.java
@@ -0,0 +1,83 @@
+package ListaEnlazada;
+
+
+public class ClaseListaEnlazada {
+	private Object info;
+	private Nodo inicio;
+	
+	public ClaseListaEnlazada(){
+		
+	}
+	
+	public void setInfo(Object o){
+		this.info=o;
+	}
+	public Object getInfo(){
+		return info;
+	}
+	
+	public void setInicio(Nodo inicio){
+		this.inicio=inicio;
+	}
+	
+	public Nodo getInicio(){
+		return this.inicio;
+	}
+	
+	public String toString(){
+		return (String)info;
+	}
+	// esto es LA MADRE DEL CORDERO
+	public void addPrincipio(Object o){
+		Nodo aux=new Nodo(o);
+		aux.setSig(inicio.getSig());
+		this.inicio.setSig(aux);
+	}
+	//ESTO ES DE CAJON 
+	public void addFinal(Object o){
+		Nodo aux=new Nodo(o);
+		Nodo recorrete=new Nodo();
+		recorrete=inicio.getSig();
+		while(recorrete.getSig()!=null){
+			recorrete=recorrete.getSig();
+		}
+		recorrete.setSig(aux);
+	}
+	//otro que se las trae
+	public boolean borrar( Object o){
+		Nodo recorrete=new Nodo();
+		recorrete=inicio.getSig();
+		while(recorrete!=null){// asi entra en el ultimo
+			if(recorrete.getSig().getO()==o){
+				//borrar
+				Nodo aux= new Nodo(o);
+				aux.setSig(recorrete.getSig());
+				recorrete.setSig(aux);
+				return true;
+			}
+			recorrete=recorrete.getSig();
+		}
+		
+		return false;
+	}
+	
+	public boolean equals(ClaseListaEnlazada c){
+		if(this.size()==c.size()){
+			return true;
+		}
+		return false;
+	}
+	
+	public int size(){
+		int c=0;
+		Nodo recorrete=new Nodo();
+		recorrete=inicio.getSig();
+		while(recorrete!=null){
+			recorrete=recorrete.getSig();
+			c++;
+		}
+		return c;
+	}
+	
+	
+}
