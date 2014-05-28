118eece609bb8d6153d85a6157bbdb6b4d23c561
jose Trincado <josenass22@gmail.com>
2014-05-28 14:12:43 +0200

    Update FyH.java

--- CartaMenu/src/Vuelos/FyH.java
+++ CartaMenu/src/Vuelos/FyH.java
@@ -9,13 +9,15 @@ public class FyH implements Comparator{
 		// comparar por fecha y luego las horas para ordenar
 		Vuelo a1=(Vuelo) o1;
 		Vuelo a2=(Vuelo) o2;
-		if(a1.getFecha().comparteTo(a2.getFecha())==1 || a1.getFecha().comparteTo(a2.getFecha())==0){
-			if(a1.getHora().comparteTo(a2.getHora())==1)
-				return 1;
-			else
-				return -1;
+		if(a1.getFecha().comparteTo(a2.getFecha())==1){
+			return 1;
 		}else if(a1.getFecha().comparteTo(a2.getFecha())==-1){
 			return -1;
+		}else{
+			if(a1.getHora().comparteTo(a2.getHora())==1)
+				return 1;
+			else if(a1.getHora().comparteTo(a2.getHora())==-1)
+				return -1;
 		}
 		return 0;
 	}
