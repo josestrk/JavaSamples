cde52a94d7620bb4825e244e1127b4bffef18dc6
Alevale <alexvales111@gmail.com>
2014-05-28 08:43:49 +0200

    incluido repetición en vuelos

--- CartaMenu/src/Vuelos/main.java
+++ CartaMenu/src/Vuelos/main.java
@@ -8,6 +8,7 @@ public class main {
 	public static void main(String[] args) {
 		Vuelo[] t1=new Vuelo[50];
 		int orden;
+		boolean p=false;
 		Scanner tlc=new Scanner(System.in);
 		t1[0]=new Vuelo(100, "Madrid", new Fecha(2,3,2014), new Hora(2,13));
 		t1[1]=new Vuelo(200, "Barcelona", new Fecha(5,3,2014), new Hora(5,13));
@@ -15,34 +16,34 @@ public class main {
 		t1[3]=new Vuelo(400, "Alcon", new Fecha(5,3,2014), new Hora(15,13));
 		
 		muestra(t1,4);
-		
-		try{
-			System.out.println("VUELOS\n\nElije método de ordenación:\n1.Por numero de Vuelo"+
-			"\n2.Por destino\n3.por Fecha&Hora\nOPCION:");
-			orden=tlc.nextInt();
-			switch (orden) {
-			case 1:
-				numVuelo numVuelo = new numVuelo();
-				ordenar(numVuelo,t1,4);
-				break;
-			case 2:
-				Destino destino = new Destino();
-				ordenar(destino,t1,4);
-				break;
-			case 3:
-				FyH fh = new FyH();
-				ordenar(fh,t1,3);
-				break;	
-			default:
-				throw new ExceptionRango();
-			}
-			
-			muestra(t1,4);
+		do{
+			try{
+				System.out.println("VUELOS\n\nElije método de ordenación:\n1.Por numero de Vuelo"+
+				"\n2.Por destino\n3.por Fecha&Hora\nOPCION:");
+				orden=tlc.nextInt();
+				switch (orden) {
+				case 1:
+					numVuelo numVuelo = new numVuelo();
+					ordenar(numVuelo,t1,4);
+					break;
+				case 2:
+					Destino destino = new Destino();
+					ordenar(destino,t1,4);
+					break;
+				case 3:
+					FyH fh = new FyH();
+					ordenar(fh,t1,3);
+					break;	
+				default:
+					throw new ExceptionRango();
+				}
 				
-		
-		}catch(ExceptionRango e){
-			System.out.println("Se salió del rango permitido");
-		}
+				muestra(t1,4);
+			}catch(ExceptionRango e){
+				System.out.println("Se salió del rango permitido. Fin de programa");
+				p=true;
+			}
+		}while(!p);	
 		
 	}
 	public static void muestra(Vuelo[] t1,int n){
