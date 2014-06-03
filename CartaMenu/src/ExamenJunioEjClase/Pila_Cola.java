06a61ab31facbf03bded91203186035658db8ac2
josestrk <josenass22@gmail.com>
2014-06-03 09:46:14 +0200

    nuevo ejercicio

---
+++ CartaMenu/src/ExamenJunioEjClase/Pila_Cola.java
@@ -0,0 +1,12 @@
+package ExamenJunioEjClase;
+/**
+ * 
+ * @author jtrincadocastan
+ *
+ *Dada una pila y dada una cola crear un método que incluya la info de una
+ *en otra.
+ *
+ */
+public class Pila_Cola {
+
+}
